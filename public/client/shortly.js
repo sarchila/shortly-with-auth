858dde8f33a932e29644d5aef57124ba687e71aa
ta3hoon <ta3hoon@gmail.com>
2013-12-14 12:35:36 -0800

    signup page now working correctly

--- public/client/shortly.js
+++ public/client/shortly.js
@@ -14,46 +14,64 @@ angular.module('shortly', ['ngRoute', 'ngCookies'])
       templateUrl: 'templates/signup.html',
       controller: 'signupController'
     })
-    .otherwise('/');
+    .when('/login',{
+      templateUrl: 'templates/signup.html',
+      controller: 'loginController'
+    })
+    .otherwise({redirectTo: '/'});
 
 })
-.controller('MainController', function($scope, $http) {
-  $http({
-    method: 'GET',
-    url: '/links'
-  }).success(function(data){
-    $scope.links = data;
-  });
-
-})
-.controller('ShortenController', function($scope, $http) {
-  $scope.linkSubmit = function(){
+.controller('MainController', function($scope, $http, $cookies, $location) {
+  if ($cookies.userToken){
     $http({
-      method: 'POST',
-      url: '/links',
-      data: { url: this.url }
+      method: 'GET',
+      url: '/links'
     }).success(function(data){
-      $scope.url = '';
-      console.log('Success!', data);
-    }).error(function(err){
-      console.log('Error!', err);
+      $scope.links = data;
     });
-  };
-}).controller('signupController', function($scope, $http, $cookies){
+  } else {
+    $location.path('/signup'); // CHANGE THIS TO LOGIN WHEN WE CREATE LOGIN
+  }
+
+})
+.controller('ShortenController', function($scope, $http, $cookies, $location) {
+  if (!$cookies.userToken){
+    $location.path('/signup'); // CHANGE THIS TO LOGIN WHEN WE CREATE LOGIN
+  } else {
+    $scope.linkSubmit = function(){
+      $http({
+        method: 'POST',
+        url: '/links',
+        data: { url: this.url }
+      }).success(function(data){
+        $scope.url = '';
+        console.log('Success!', data);
+      }).error(function(err){
+        console.log('Error!', err);
+      });
+    };
+  }
+})
+
+.controller('signupController', function($scope, $http, $cookies, $location){
+
+  // Check if user is logged in.
+  if($cookies.userToken) {
+    $location.path('/');
+  }
+
   $scope.postNewUser = function(){
+    // console.log(this);
+
     $http({
       method: 'POST',
-      url: '/api/signup',
-      data: {
-        username: this.userName,
-        password: this.userPass
-      }
+      url: '/api/signup?username=' + this.userName + '&password=' + this.userPass,
     }).success(function(data, status, headers, config) {
       console.log('Success!', data);
       // set cookie data.token
       $cookies.userToken = data.token;
       //redirect somewhere.
-
+      $location.path("/");
 
     }).error(function(err){
       console.log('Error!', err);
--- shortly.rb
+++ shortly.rb
@@ -131,17 +131,17 @@ post '/signup' do
 end
 
 post '/api/signup' do
-    # user = User.find_by_username(params[:username])
-    # unless user.nil?
-        # [403, {error: 'User not found'}.to_json]
-    # else
+    user = User.find_by_username(params[:username])
+    unless user.nil?
+        [403, {error: 'User not found'}.to_json]
+    else
         user = User.create username: params[:username], password: params[:password]
         if user.valid?
             [200, {token: user.identifier}.to_json]
         else
             [401, {error: user.valid?}.to_json]
         end
-    # end
+    end
 end
 
 get "/logout" do
